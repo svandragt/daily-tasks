810a15c22cda061cc6222acd8f996c8711ebc296
Sander van Dragt <sander@vandragt.com>
2024-06-21 11:38:15 +0100

    Local htmx

--- src/index.php
+++ src/index.php
@@ -40,7 +40,7 @@ if ($_POST) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Daily Tasks</title>
-    <script src="https://cdn.jsdelivr.net/npm/htmx.org/dist/htmx.min.js"></script>
+    <script src="htmx.min.js?2.0.0"></script>
     <style>
         input[type=checkbox] {
             cursor: pointer;
