028d6e52b829e7bc0775fe85c63e897468fd9f71
Sander van Dragt <sander@vandragt.com>
2024-06-21 14:15:12 +0100

    Favicon (thanks gpt4o)

--- src/index.php
+++ src/index.php
@@ -38,6 +38,7 @@ if ($_POST) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <title>Three Tasks for <?= $date ?></title>
     <script src="htmx.min.js?1.9.2"></script>
     <script src="idiomorph-ext.min.js"></script>
